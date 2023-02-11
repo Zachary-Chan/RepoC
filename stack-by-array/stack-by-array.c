6fd573ae83df0d87bfc9d6f514d599f2345d9d0e
Ziyan <ziyan.cheng@outlook.it>
2023-02-11 22:40:22 +0100

    Added stack-by-array

---
+++ stack-by-array/stack-by-array.c
@@ -0,0 +1,56 @@
+#include "stack-by-array.h"
+#include <stdio.h>
+
+// simply let top equal to -1
+Stack *initialize(Stack *stack)
+{
+    stack->top = -1;
+    return stack;
+}
+
+void push(Stack *stack, DATA_TYPE value)
+{
+    // special case, stack is full
+    if(stack->top == SIZE - 1){
+        return;
+    }
+
+    // general case
+    stack->top++;
+    stack->stack[stack->top] = value;
+}
+
+DATA_TYPE pop(Stack *stack)
+{
+    // special case, stack is empty 
+    if (stack->top == -1){
+        return -1;
+    }
+
+    DATA_TYPE return_value = stack->stack[stack->top];
+    stack->top--;
+    return return_value;
+}
+
+DATA_TYPE peek(Stack *stack)
+{
+    // if empty 
+    if(stack->top == -1){
+        return -1;
+    }
+
+    return stack->stack[stack->top];
+}
+
+void displayInt(Stack *stack)
+{
+    // if empty 
+    if (stack->top == -1){
+        return;
+    }
+
+    for(int i = stack->top; i >= 0; i--) {
+        printf("[%d] ", stack->stack[i]);
+    }
+    puts("");
+}
---
+++ stack-by-array/stack-by-array.h
@@ -0,0 +1,17 @@
+#ifndef STACK_BY_ARRAY_H
+#define STACK_BY_ARRAY_H
+
+#define SIZE 5
+typedef int DATA_TYPE;
+typedef struct Stack {
+    int top;
+    DATA_TYPE stack[SIZE];
+}Stack;
+
+Stack* initialize(Stack* stack);
+void push(Stack* stack, DATA_TYPE value);
+DATA_TYPE pop(Stack* stack);
+DATA_TYPE peek(Stack* stack);
+void displayInt(Stack* stack);
+
+#endif
